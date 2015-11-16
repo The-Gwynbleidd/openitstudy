741640737be4a06f0cea7d8ac764dccb73649e8a
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2015-11-16 22:07:49 +0100

    Add username for login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,37 @@
 class User < ActiveRecord::Base
+  # Data Validations
+  validates :username,
+    :presence => true,
+    :uniqueness => {
+      :case_sensitive => false
+  }
+
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
   # User Authentication
   ## Include default devise modules. Others available are:
   ## :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Use username for login with devise
+  attr_accessor :login
+
+
+  def self.find_for_database_authentication(warden_conditions)
+     conditions = warden_conditions.dup
+     if login = conditions.delete(:login)
+       where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+     else
+       where(conditions.to_h).first
+     end
+   end
 
   # User communication
   acts_as_followable
